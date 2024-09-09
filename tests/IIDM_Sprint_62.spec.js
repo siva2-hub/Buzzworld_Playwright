fe9f33c3b9fe07ccbd021c9834c1965dc0214720
siva2-hub1 <sivadara17@gmail.com>
2024-09-09 18:58:22 +0530

    multiple items are added to RMA

--- tests/IIDM_Sprint_62.spec.js
+++ tests/IIDM_Sprint_62.spec.js
@@ -1,5 +1,5 @@
 const { test } = require("@playwright/test");
-const { itemNotesLineBreaks, verifySPAExpiryMails, returnResult, setScreenSize, login_buzz, addTerritoryToZipcodes, defaultTurnAroundTime, getImages, orgSearchLoginAsClient, loginAsClient, quoteTotalDisplaysZero, displayNCNRatItemsPage, salesOrderVerification } = require("./helper");
+const { itemNotesLineBreaks, verifySPAExpiryMails, returnResult, setScreenSize, login_buzz, addTerritoryToZipcodes, defaultTurnAroundTime, getImages, orgSearchLoginAsClient, loginAsClient, quoteTotalDisplaysZero, displayNCNRatItemsPage, salesOrderVerification, addMultipleItems } = require("./helper");
 const testdata = JSON.parse(JSON.stringify(require("../testdata.json")));
 // const stage_url = testdata.urls.buzz_dev_url;
 const stage_url = process.env.BASE_URL_BUZZ;
@@ -25,6 +25,12 @@ test('Display NCNR at Items Details', async ({ }, testInfo) => {
 });
 test('8 series sales order verification', async ({ }, testInfo) => {
   results = await salesOrderVerification(page);
+  let testName = testInfo.title;
+  await returnResult(page, testName, results);
+});
+test('add multiple items to RMA', async ({ }, testInfo) => {
+  let stock_codes=['NS15-TX01B-V2', 'S4K2U3000-5C', 'BARRACUDASA-19', 'BARRACUDASA-19', 'BARRACUDASA-19', '6016T-MTHF', '6016T-MTHF', '6016T-MTHF', '6016T-MTHF', 'S4K4U6000W0BATC'];
+  results = await addMultipleItems(page, stock_codes, );
   let testName = testInfo.title;
   await returnResult(page, testName, results);
 });
