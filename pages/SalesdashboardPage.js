b4588003083684694c9fc66fb41b91bde42ed1e1
siva2-hub1 <sivadara17@gmail.com>
2025-05-16 19:02:58 +0530

    Check the branched for different user roles

--- pages/SalesdashboardPage.js
+++ pages/SalesdashboardPage.js
@@ -1,6 +1,7 @@
 import { expect } from "@playwright/test";
 import { getEleByText } from "./PricingPages";
-import { delay, search_user } from "../tests/helper";
+import { ANSI_ORANGE, ANSI_RESET, delay, login_buzz, login_buzz_newUser, search_user, selectReactDropdowns } from "../tests/helper";
+import { reactFirstDropdown } from "./PartsBuyingPages";
 export const ytdSalesTarget = (page) => { return page.locator("//*[@class='appointments-target']") }
 export const filterArrow = (page) => { return page.locator("//*[@class='arrow']") }
 export const selectBranch = (page, branchName) => { return page.getByRole('button', { name: `${branchName} Expand node` }) }
@@ -9,6 +10,10 @@ export const selectSalesPerson = (page, salesPerson) => { return page.getByRole(
 export const saveButton = (page) => { return page.getByRole('button', { name: 'Save' }) }
 export const salesButton = (page) => { return page.locator("//*[text()='Sales' and @class='link-icon-text']") }
 export const avgGoalsSalesDashB = (page) => { return page.locator("(//*[contains(@class,'recharts-reference-line')])[2]") }
+export const userEdit = (page) => { return page.locator("(//*[contains(@src,'themecolorEdit')])[1]") }
+export const userUpdateBtn = (page) => { return page.getByRole('button', { name: 'Update' }) }
+export const userProfileIcon = (page) => { return page.locator("//*[@class='user_image']") }
+export const branchesListSD = (page) => { return page.locator("//*[@class='tree-select-dropdown']/div/div") }
 
 export async function getYTDTargets(page, salesPerson) {
     await getEleByText(page, 'Dashboard').nth(0).click();
@@ -16,7 +21,7 @@ export async function getYTDTargets(page, salesPerson) {
     await filterArrow(page).nth(0).click(); await delay(page, 4000);
     //*[@class='tree-select-dropdown']/div/div[1]/div[2]/div
     for (let s = 0; s < salesPerson.length; s++) {
-        let branches = await page.locator("//*[@class='tree-select-dropdown']/div/div");
+        let branches = await branchesListSD(page);
         console.log('branches count is ', await branches.count())
         for (let index = 0; index < await branches.count(); index++) {
             let sPerson = await branches.nth(index).locator("//div[2]/div");
@@ -110,4 +115,61 @@ export async function checkYTDSalesTarget(page, months, salesPerson, appendValue
         testResult = false;
     }
     return testResult;
+}
+export async function checkBranchesForSuperUserInSalesDashboard(page, browser, userData, newPage) {
+    let [url, userEmail, pWord, userRole, branchName, count] = userData, testResult = false;
+    await search_user(page, userEmail);
+    await getEleByText(page, 'User Profile').nth(0).click();
+    await userEdit(page).click();
+    //change user role
+    await reactFirstDropdown(page).nth(1).click();
+    await selectReactDropdowns(page, userRole);
+    //change branch
+    await reactFirstDropdown(page).nth(3).click();
+    await selectReactDropdowns(page, branchName);
+    await userUpdateBtn(page).nth(0).click();
+    await expect(page.locator('div').filter({ hasText: /^Updated Successfully$/ }).nth(2)).toBeVisible();
+    await delay(page, 2300);
+    let context;
+    if (count == 0) {
+        context = await browser.newContext();
+        newPage = await context.newPage();
+        await login_buzz_newUser(newPage, url, userEmail, pWord);
+    }
+    await userProfileIcon(newPage).click()
+    await getEleByText(newPage, 'User Profile').nth(0).click();
+    if (userRole != 'Sales') { await expect(getEleByText(newPage, userEmail)).toBeVisible(); } await delay(newPage, 1500);
+    await getEleByText(newPage, 'Dashboard').nth(0).click();
+    await salesButton(newPage).click(); await delay(newPage, 1200);
+    if (await filterArrow(newPage).count() > 0) {
+        await filterArrow(newPage).nth(0).click(); await delay(newPage, 4000);
+    } else { console.log(`branches filter arrow not displaying for user role ${ANSI_ORANGE}${userRole}${ANSI_RESET}`) }
+    let branchesCount = await branchesListSD(newPage).count();
+    console.log(`branches count is ${branchesCount} for user role ${ANSI_ORANGE}${userRole}${ANSI_RESET}`)
+    console.log('Displaying branches at dropdowns are:'); let branchNameSD;
+    if (userRole != 'Sales') {
+        for (let index = 0; index < branchesCount; index++) {
+            branchNameSD = await branchesListSD(newPage).nth(index).locator("//div[1]").nth(0).textContent()
+            console.log(`${branchNameSD}`)
+        }
+    } else { console.log(`branch is not display for user role ${ANSI_ORANGE}${userRole}${ANSI_RESET}`) }
+    switch (userRole) {
+        case 'Super User':
+            if (branchesCount == 7) { testResult = true; }
+            else { testResult = false; }
+            break;
+        case 'Sales Manager':
+            if (branchesCount == 1) {
+                if (branchName == branchNameSD) { console.log(`set branch for user is ${branchName}\ndisplaying branch at sals dashboard is ${branchNameSD}`); testResult = true; }
+            } else { testResult = false; }
+            break;
+        case 'Sales':
+            if (branchesCount == 0) { testResult = true; }
+            else { testResult = false; }
+            break;
+        default:
+            console.log(`${userRole} not checking`)
+            break;
+    }
+    return [testResult, newPage];
 }
--- tests/SalesDashboardTests.spec.js
+++ tests/SalesDashboardTests.spec.js
@@ -1,8 +1,9 @@
 import test, { chromium } from "@playwright/test";
 import { login_buzz } from "./helper";
 import { testData } from "../pages/TestData";
-import { checkYTDSalesTarget } from "../pages/SalesdashboardPage";
+import { checkBranchesForSuperUserInSalesDashboard, checkYTDSalesTarget } from "../pages/SalesdashboardPage";
 import { getTestResults } from "../pages/PricingPages";
+import { stage_url } from "../pages/QuotesPage";
 
 let page, results, browser;
 test.beforeAll(async () => {
@@ -17,4 +18,30 @@ test('check YTD Sales Target', async ({ }, testInfo) => {
     let salesPerson = ['Michael Smith', 'Will Gray'];
     results = await checkYTDSalesTarget(page, months, salesPerson, 200001.17);
     await getTestResults(results, testInfo);
+})
+test.describe('Check the Branches in Dashboard for Different User Roles', async () => {
+    let userData = [
+        process.env.BASE_URL_BUZZ, 'testdefault@epi.com', 'Enter@4321', 'Super User', 'Dallas', 1
+    ];
+    test('Check the Branches in Dashboard for Super User', async ({ }, testInfo) => {
+        // let userData = [secondPageURL, userEmail, pWord, 'Super User', 'Dallas', count];
+        // let [url, userEmail, pWord, userRole, branchName] = userData;
+        userData.splice(5, 1, 0);
+        results = await checkBranchesForSuperUserInSalesDashboard(page, browser, userData, results);
+        await getTestResults(results[0], testInfo)
+    })
+    test('Check the Branches in Dashboard for Sales Manager', async ({ }, testInfo) => {
+        // let userData = [secondPageURL, userEmail, pWord, 'Sales Manager', 'San Antonio', 1];
+        // let [url, userEmail, pWord, userRole, branchName] = userData;
+        userData.splice(3, 1, 'Sales Manager'); userData.splice(4, 1, 'San Antonio'); userData.splice(5, 1, 1);
+        results = await checkBranchesForSuperUserInSalesDashboard(page, browser, userData, results[1]);
+        await getTestResults(results[0], testInfo)
+    })
+    test('Check the Branches in Dashboard for Sales', async ({ }, testInfo) => {
+        // let userData = [secondPageURL, userEmail, pWord, 'Sales', 'Chicago', 1];
+        // let [url, userEmail, pWord, userRole, branchName] = userData;
+        userData.splice(3, 1, 'Sales'); userData.splice(4, 1, 'Chicago');
+        results = await checkBranchesForSuperUserInSalesDashboard(page, browser, userData, results[1]);
+        await getTestResults(results[0], testInfo)
+    })
 })
